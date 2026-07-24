ef1d89cabb0ebce1ee186a2d7423f93f37901322
4ndr3so <lp.carlosorjuela@gmail.com>
2026-07-24 12:01:55 -0400

    vercel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.2.88',
+    host: '192.168.1.68',
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    emptyOutDir: true,
   },
 })
+
